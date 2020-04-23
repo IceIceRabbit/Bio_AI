17cf06fab9f908adf0cd063999eec26ac6487e65
m-ayala <60243227+m-ayala@users.noreply.github.com>
2020-04-22 18:32:25 -0700

    Update brain.py

--- Team_Formation_Python/brain.py
+++ Team_Formation_Python/brain.py
@@ -1,18 +1,23 @@
-import .world
-import .MGA
-class Brain:
-    def __init__(self,grid_length,team_size,actions):
+import sys
+sys.path.insert(0, r'C:\Users\manas\Documents\Codes\BioAI\Final')
+import world
+from MGA import MGA
+
+class brain:
+    
+    def __init__(self,grid_length,team_size,actions, agent_states, grid_x, grid_y):
         # self.gates = []
         self.grid_reward = [0 for x in range(grid_length)]
         self.team_size = team_size
         self.actions = actions
         self.friends = 0
-        
+        self.mga = MGA(grid_length, self.grid_reward, self.actions, agent_states, grid_x, grid_y)
+
+
     def get_action(self,state):
-        action = MGA.get_action(state)
+        
+        action = self.mga.move(0.9, state)
         return action
-
-
 
 
     def reward_gate(self,inputs):
@@ -22,7 +27,3 @@ class Brain:
         if friendlies:
             net_reward = pow(team + 1,friendlies)
         return net_reward
-
-
-
-
